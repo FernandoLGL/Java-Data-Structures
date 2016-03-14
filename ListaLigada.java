bb5ca826131958a5e5cf4036f3f856861fc092d5
FernandoLGL <fernando.neto.97@hotmail.com>
2016-03-14 17:43:36 -0300

    Update ListaLigada.java

--- ListaLigada.java
+++ ListaLigada.java
@@ -1,7 +1,6 @@
 
 public class ListaLigada {
 	private Node inicio;
-	private int contador = 0;
 	
 	public ListaLigada(){
 		
@@ -10,35 +9,77 @@ public class ListaLigada {
 	public ListaLigada(int info){
 		Node novo = new Node(info);
 		inicio = novo;
-		contador++;
 	}
 
 	public void adicionar(int info){
+		//Node da info
 		Node novo = new Node(info);
-
-		if(contador == 0){
+		//ver se a lista ta vazia
+		if (inicio == null) {
 			inicio = novo;
-			contador++;
-		}else{
-			inicio.next = novo;
-			contador++;
+		}
+		//se a lista tiver apenas 1 elemento
+		else if (inicio.getNext() == null) {
+			inicio.setNext(novo);
+		} 
+		// se a lista tiver +1 elemento
+		else {
+				Node aux = inicio.getNext();
+				//percorrendo a lista atras do ultimo elemento
+				while (aux.getNext() != null) {
+					aux = aux.getNext();
+				}
+				aux.setNext(novo);
+
 		}
 	}
-	public void remover(){
+	public void remover(int k){
 		
 	}
 	public void imprimir(){
-		for(int i = 0; i < contador; i++){
-			System.out.println();
+		
+		if(inicio == null){
+			System.out.println("A lista esta vazia!!");
 		}
+		else{
+			Node aux = inicio.getNext();
+			System.out.println(inicio.getInfo());
+			//percorrendo a lista atras do ultimo elemento
+			while (aux != null) {
+				System.out.println(aux.getInfo());
+				aux = aux.getNext();
+			}
+		}
+
 	}
 	public void esvaziar(){
 		
-	}
-	public void acharIesimo(){
+		if(inicio == null){
+			// Pra quem estiver lendo isso, vai ter um Kirby novo com robô
+			System.out.println("A lista ja esta vazia!!");
+		}
+		else{
+			// Matando a jararaca pela cabeça
+			inicio = null;
+		}
 		
 	}
-	public void consultar(){
+	public int acharIesimo(int k){
+		return 0;
+	}
+	public void consultar(int k){
 		
+	}
+	
+	public static void main(String[] args) {
+		ListaLigada l1 = new ListaLigada(2);
+		l1.adicionar(60);
+		l1.adicionar(3);
+		l1.adicionar(5);
+		l1.adicionar(8);
+		l1.adicionar(9);
+		l1.imprimir();
+		l1.esvaziar();
+		l1.imprimir();
 	}
 }
