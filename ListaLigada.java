8c75f6edfdd104d0452df96049d06551ff32f2c3
Wardaniel <danielvp15@live.com>
2016-03-17 17:55:32 -0300

    Update ListaLigada.java

--- ListaLigada.java
+++ ListaLigada.java
@@ -1,64 +1,54 @@
-
 public class ListaLigada {
 	private Node inicio;
-	
-	public ListaLigada(){
-		
+
+	public ListaLigada() {
+
 	}
 
-	public ListaLigada(int info){
+	public ListaLigada(int info) {
 		Node novo = new Node(info);
 		inicio = novo;
 	}
-	
-	public void ordenar(){
-		if(inicio != null){
-			return;
-		}else if(inicio.getNext() == null){
-			return;
-		}else{
-			//codigo para ordenar
-		}	
-	}
 
-	public void adicionar(int info){
-		//Node da info
+	public void adicionar(int info) {
+		// Node da info
 		Node novo = new Node(info);
-		//ver se a lista ta vazia
+		// ver se a lista ta vazia
 		if (inicio == null) {
 			inicio = novo;
 		}
-		//se a lista tiver apenas 1 elemento
+		// se a lista tiver apenas 1 elemento
 		else if (inicio.getNext() == null) {
 			inicio.setNext(novo);
-		} 
+		}
 		// se a lista tiver +1 elemento
 		else {
-				Node aux = inicio.getNext();
-				//percorrendo a lista atras do ultimo elemento
-				while (aux.getNext() != null) {
-					aux = aux.getNext();
-				}
-				aux.setNext(novo);
+			Node aux = inicio.getNext();
+			// percorrendo a lista atras do ultimo elemento
+			while (aux.getNext() != null) {
+				aux = aux.getNext();
+			}
+			aux.setNext(novo);
 
 		}
-		ordenar();
 	}
+
 	public void remover(int k){
-		Node novo;
-		novo = acharIesimo(k);
-		novo = null;
-		ordenar();
+		Node teste1;
+		Node teste2;
+		teste1 = acharIesimo(k-1);
+		teste2 = acharIesimo(k+1);
+		teste1.setNext(teste2);
 	}
-	public void imprimir(){
-		
-		if(inicio == null){
+
+	public void imprimir() {
+
+		if (inicio == null) {
 			System.out.println("A lista esta vazia!!");
-		}
-		else{
+		} else {
 			Node aux = inicio.getNext();
 			System.out.println(inicio.getInfo());
-			//percorrendo a lista atras do ultimo elemento
+			// percorrendo a lista atras do ultimo elemento
 			while (aux != null) {
 				System.out.println(aux.getInfo());
 				aux = aux.getNext();
@@ -66,28 +56,54 @@ public class ListaLigada {
 		}
 
 	}
-	public void esvaziar(){
-		
-		if(inicio == null){
+
+	public void esvaziar() {
+
+		if (inicio == null) {
 			// Pra quem estiver lendo isso, vai ter um Kirby novo com robô
 			System.out.println("A lista ja esta vazia!!");
-		}
-		else{
+		} else {
 			// Matando a jararaca pela cabeça
 			inicio = null;
 		}
-		
+
 	}
-	public Node acharIesimo(int k){
-		return null;
+
+	public Node acharIesimo(int k) {
+		if (inicio == null) {
+			// Pra quem estiver lendo isso, vai ter um Kirby novo com robô
+			System.out.println("A lista esta vazia!!");
+			return  null;
+		} else {
+			Node aux = inicio;
+			int contador = 1;
+			// percorrendo a lista atras do ultimo elemento
+			while (aux != null && contador != k) {
+				aux = aux.getNext();
+				contador++;
+			}
+			return aux;
+		}
 	}
-	public void consultar(int k){
-		System.out.println("O item eh o seguinte:\n\nInformacao contida:" + acharIesimo(k).getInfo());
-		System.out.println("O node eh: " + acharIesimo(k).toString());
-		System.out.println("O proximo node eh: " + acharIesimo(k).getNext().toString());
-		
+
+	public void consultar(int k) {
+		if (inicio == null) {
+			// Pra quem estiver lendo isso, vai ter um Kirby novo com robô
+			System.out.println("A lista esta vazia!!");
+		} else {
+			Node aux = inicio;
+			int aux2 = inicio.getInfo();
+			int contador = 1;
+			// percorrendo a lista atras do ultimo elemento
+			while (aux != null && aux2 != k) {
+				aux = aux.getNext();
+				aux2 = aux.getInfo();
+				contador++;
+			}
+			System.out.println("O nó esta na posição: " + contador +"\nSua informação é: " + aux2 + "\nO proximo é: " + aux.getNext());
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		ListaLigada l1 = new ListaLigada(2);
 		l1.adicionar(60);
@@ -96,7 +112,11 @@ public class ListaLigada {
 		l1.adicionar(8);
 		l1.adicionar(9);
 		l1.imprimir();
+		l1.consultar(7);
+		l1.remover(9);
+		l1.imprimir();
 		l1.esvaziar();
 		l1.imprimir();
+		l1.consultar(2);
 	}
 }
