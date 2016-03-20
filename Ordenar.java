a8e84ab792935b3608ca389decd8ec878b62970a
Wardaniel <danielvp15@live.com>
2016-03-20 13:55:00 -0300

    Create Ordenar.java

---
+++ Ordenar.java
@@ -0,0 +1,26 @@
+public void ordernar() {
+		for (int z = 0; z < 15; z++) {
+			int aux;
+			if (repositorio[z] > repositorio[z + 1]) {
+				if (repositorio[z + 1] == 0) {
+				} else {
+					aux = repositorio[z];
+					repositorio[z] = repositorio[z + 1];
+					repositorio[z + 1] = aux;
+					for (int i = z; i != 0; i--) {
+						if (repositorio[z - i] > repositorio[z]) {
+							aux = repositorio[z - i];
+							repositorio[z - i] = repositorio[z];
+							repositorio[z] = aux;
+						}
+
+					}
+
+				}
+			}
+
+		}
+		for (int x = 0; x < 10; x++) {
+			System.out.println(repositorio[x]);
+		}
+	}
