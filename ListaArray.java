fc9dbfaaab99a4af88ba350b31da684dbed8538e
FernandoLGL <fernando.neto.97@hotmail.com>
2016-03-22 17:41:41 -0300

    Update ListaArray.java

--- ListaArray.java
+++ ListaArray.java
@@ -1,27 +1,48 @@
+package lista;
+
+//imports
 import java.util.Scanner;
 
-
+import excecoes.*;
+//											@todo Consultar
 public class ListaArray {
-	
+	//atributos
 	private Integer[] repositorio;
 	private int contador;
+	private int tamanho;
 	
+	//construtor1
 	public ListaArray(){
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Qual vai ser o tamanho do array?");
-		int tamanho = sc.nextInt();
+		tamanho = sc.nextInt();
+		
+		if(tamanho == 0){
+			System.out.println("O seu array nao pode ser de tamanho 0! O tamanho padrao(5) foi selecionado.");
+			tamanho = 5;
+		}
+		
 		repositorio = new Integer[tamanho];
 	}
-	
+	//construtor2
 	public ListaArray(int info){
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Qual vai ser o tamanho do array?");
-		int tamanho = sc.nextInt();
+		tamanho = sc.nextInt();
+		
+		if(tamanho == 0){
+			System.out.println("O seu array nao pode ser de tamanho 0! O tamanho padrao(5) foi selecionado.");
+			tamanho = 5;
+		}
+		
 		repositorio = new Integer[tamanho];
 		repositorio[0] = info;
 		contador++;
+		
 	}
-	
+	//algoritmo de ordenacao
 	public void ordenar() {
 		for (int z = 0; z < 15; z++) {
 			int aux;
@@ -45,56 +66,81 @@ public class ListaArray {
 		}
 	}
 	
-	public int buscaBinaria(int k){
-		return 0;
-	}
-	
-	public void imprimir(){
+	//metodo imprimir
+	public void imprimir() throws ListaVaziaException{
+		
 		if(contador == 0){
-			System.out.println("A lista esta vazia.");
+			throw new ListaVaziaException("A lista se encontra vazia");
 		}
-		ordenar();
-		for(int i = 0; i <= contador; i++){
+		
+		//ordenar();
+		System.out.println("A lista eh: ");
+		for(int i = 0; i < contador; i++){
 			if(repositorio[i] != null){
-				System.out.println(repositorio[i]);
+				System.out.print(repositorio[i] + " ");
 			}
 		}
+		
 	}
-	
-	public void esvaziar(){
+	//metodo esvaziar
+	public void esvaziar() throws ListaVaziaException{
+		
 		if(contador == 0){
-			System.out.println("A lista ja esta vazia.");
+			throw new ListaVaziaException("A lista ja esta vazia.");
 		}
+		
 		for(int i = 0; i < contador; i++){
 			repositorio[i] = null;
 		}
+		
 		contador = 0;
-		System.out.println("Esvaziou");
+		System.out.println("Lista esvaziada.");
 	}
-	
-	public void adicionar(int k){
+	//metodo adicionar
+	public void adicionar(int k) throws ListaCheiaException{
+		
+		if(contador == tamanho){
+			throw new ListaCheiaException("A lista esta cheia.");
+		}
+		
 		repositorio[contador] = k;
 		contador++;
-		ordenar();
+		//ordenar();
 	}
 	
-	public void consultar(int k){
-		ordenar();
-		System.out.println("O valor dessa posicao eh: " + repositorio[k - 1]);
-		System.out.println("O proximo valor eh: " + repositorio[k]);
-		System.out.println("O valor anterior eh: " + repositorio[k - 2]);
+	//metodo consultar implementado com Busca Binaria
+	public int consultar(int k) throws ValorInvalidoException{
+		//ordenar();
+		int comeco = 0;
+		int topo = contador - 1;
+		
+		while(comeco <= topo){
+			if(repositorio[comeco] == k){
+	        	return repositorio[comeco];
+			}else if(repositorio[comeco] > k){
+				topo--;
+			}else if(repositorio[comeco] < k){
+				comeco++;
+			}
+		}
+		throw new ValorInvalidoException("O valor eh invalido.");
 	}
-	
-	public int acharIesimo(int k){
-		ordenar();
+	//metodo acharIesimo
+	public int acharIesimo(int k) throws ValorInvalidoException{
+		
+		if( (k <= 0) || (k > tamanho) || (repositorio[k-1] == null) ){
+			throw new ValorInvalidoException("Valor invalido!");
+		}
+		
+		//ordenar();
+		
 		return repositorio[k-1];
 	}
-	
-	public void remover(int k){
-		ordenar();
+	//metodo remover
+	public void remover(int k) throws ListaVaziaException{
+		//ordenar();
 		if(contador == 0){
-			System.out.println("A lista esta vazia.");
-			return;
+			throw new ListaVaziaException("A lista esta vazia.");
 		}
 		
 		for(int i = 0; i < contador; i++){
@@ -108,14 +154,4 @@ public class ListaArray {
 		System.out.println("Invalido!!");
 		contador--;
 	}
-	
-	public static void main(String[] args){
-		ListaArray la = new ListaArray(2);
-		la.adicionar(5);
-		la.adicionar(7);
-		la.adicionar(4);
-		la.remover(7);
-		la.consultar(3);
-		la.imprimir();
-	}
-}
+}	
