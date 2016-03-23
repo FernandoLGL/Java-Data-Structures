f60bacf3f3658d6e9374637cd4e00befb5df4fc6
FernandoLGL <fernando.neto.97@hotmail.com>
2016-03-23 14:08:24 -0300

    Create MenuLigada.java

---
+++ MenuLigada.java
@@ -0,0 +1,64 @@
+package menu;
+//imports
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import excecoes.ListaVaziaException;
+import excecoes.NodeInexistenteException;
+import excecoes.NodeJaExistenteException;
+import lista.*;
+//
+public class Menu {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		Scanner sc = new Scanner(System.in);
+		ListaLigada ll = new ListaLigada();
+		int escolha;
+		int numero;
+		while(true){
+			System.out.println("\n\n1 - Adicionar");
+			System.out.println("2 - Remover");
+			System.out.println("3 - Consultar");
+			System.out.println("4 - Achar I-esimo elemento");
+			System.out.println("5 - Esvaziar");
+			System.out.println("6 - Imprimir");
+			try{
+				escolha = sc.nextInt();
+				switch(escolha){
+					case 1: System.out.println("Digite o numero que deseja adicionar");
+					numero = sc.nextInt();
+					ll.adicionar(numero);
+					break;
+					
+					case 2: System.out.println("Digite o numero que deseja remover");
+					numero = sc.nextInt();
+					ll.remover(numero);
+					break;
+					
+					case 3: System.out.println("Digite o valor que deseja consultar");
+					numero = sc.nextInt();
+					ll.consultar(numero);
+					break;
+					
+					case 4: System.out.println("Digite a posicao do elemento");
+					numero = sc.nextInt();
+					System.out.println(ll.acharIesimo(numero));
+					break;
+					
+					case 5: ll.esvaziar();
+					break;
+					
+					case 6: ll.imprimir();
+					break;
+					
+					default: System.out.println("Opcao invalida!");
+					break;
+				}
+			}catch(InputMismatchException | ListaVaziaException | NodeJaExistenteException | NodeInexistenteException e){
+				System.out.println(e.getMessage());
+			} 
+		}
+	}
+
+}
