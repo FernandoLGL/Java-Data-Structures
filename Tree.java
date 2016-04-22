665341acf06d7c81397d384a644cd16f91b1b293
Wardaniel <danielvp15@live.com>
2016-04-22 14:02:16 -0300

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -1,44 +1,57 @@
 
 public class Tree {
-	//ATRIBUTO SHOW
+	// ATRIBUTO SHOW
 	public NodeTree raiz;
 
-	//METODOS SHOWS
-	public void insert(NodeTree galho) {
-		//AUXILIAR SHOW
+	// METODOS SHOWS
+	public void insert(int number) {
+		// AUXILIAR SHOW
+		NodeTree galho = new NodeTree(number);
 		NodeTree aux = raiz;
-		//CHECAGEM SHOW
+		// CHECAGEM SHOW
 		if (raiz == null) {
 			raiz = galho;
-			System.out.println(raiz.getInfo());
+			System.out.println(galho.getInfo() +" raiz");
 		} else {
-			//#PARTIUPERCORRERARVORE
+			// #PARTIUPERCORRERARVORE
 			do {
-				if (galho.getInfo() > aux.getInfo()) {
-					//#INDOPRADIREITA
+				if (number > aux.getInfo()) {
+					// #INDOPRADIREITA
 					if (aux.getRightSide() == null) {
 						aux.setRightSide(galho);
-						System.out.println(aux.getInfo() + " galho direito " + galho.getInfo());
+						System.out.println(aux.getInfo() + " direita ->" + galho.getInfo() + " fim");
 						return;
 					} else {
-						//VOLTANDO PRO LOOP
-						System.out.println(aux.getInfo() + " indo para no a direita " + aux.getRightSide().getInfo());
+						// VOLTANDO PRO LOOP
+						System.out.println(aux.getInfo() + " direita ->" + aux.getRightSide().getInfo());
 						aux = aux.getRightSide();
 					}
 				} else {
-					//#INDOPRAESQUERDA(ECA FORA PT)
+					// #INDOPRAESQUERDA(ECA FORA PT)
 					if (aux.getLeftSide() == null) {
 						aux.setLeftSide(galho);
-						System.out.println(aux.getInfo() + " galho esquerdo " + galho.getInfo());
+						System.out.println(aux.getInfo() + " esquerda ->" + galho.getInfo()+ " fim");
 						return;
 					} else {
-						//VOLTANDO PRO LOOP
-						System.out.println(aux.getInfo() + " indo para no a esquerda " + aux.getLeftSide().getInfo());
+						// VOLTANDO PRO LOOP
+						System.out.println(aux.getInfo() + " esquerda ->" + aux.getLeftSide().getInfo());
 						aux = aux.getLeftSide();
 					}
 				}
-			}//LOOP INFINITO SHOW 
+			} // LOOP INFINITO SHOW
 			while (true);
 		}
 	}
-}
+
+	public void remove(int node) {
+		NodeTree dad;
+		NodeTree filho;
+		dad = raiz;
+		if (node == dad.getInfo()) {
+			if (dad.getLeftSide() == null && dad.getRightSide() == null) {
+				dad = null;
+			}
+		} else if (dad.getInfo() > node) {
+		}
+	}
+
